3a7820844bca3734a86627e60a0918fbe5c27b4d
root <aghababayevarif@gmail.com>
2025-09-16 21:01:23 +0400

    third commit

--- server-stats.sh
+++ server-stats.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "Hello World"
+FILE=/root/users
+echo "today is $(date)"
+echo "I am going to add new users to system which is listed /root/users file"
+while IFS= read -r line;do
+        if grep -Fxq "$line" "/etc/passwd";then
+                echo "User wasn't created because it already exists"
+        else
+
+                adduser $line
+        fi
+
+done < "$FILE"
+
