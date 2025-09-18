59010bea847b8635c350fa51fb301a2b16bacee4
root <aghababayevarif@gmail.com>
2025-09-18 22:12:24 +0400

    4th commit

--- server-stats.sh
+++ server-stats.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-FILE=/root/users
-echo "today is $(date)"
-echo "I am going to add new users to system which is listed /root/users file"
-while IFS= read -r line;do
-        if grep -Fxq "$line" "/etc/passwd";then
-                echo "User wasn't created because it already exists"
-        else
-
-                adduser $line
-        fi
-
-done < "$FILE"
-
+stats=/usr/bin/top
+ 
+$stats | grep -E --color=never "%Cpu|buff/cache"
+df -h | awk '{print $1,$4,$5}' | column -t
