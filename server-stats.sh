588d9a7ef49e83259c50355fe0ee9e3f428c73f4
root <aghababayevarif@gmail.com>
2025-09-25 20:42:47 +0400

    final rush

--- server-stats.sh
+++ server-stats.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 stats=/usr/bin/top
- 
 $stats | grep -E --color=never "%Cpu|buff/cache"
+
 df -h | awk '{print $1,$4,$5}' | column -t
+top | ps -aux | awk '{print $1,$3,$4,$11}' | head -6
