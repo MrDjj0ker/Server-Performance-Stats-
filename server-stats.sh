ce431c9413e815de86c2eb91f9044063377ad9ec
root <aghababayevarif@gmail.com>
2025-10-05 21:26:37 +0400

    LAST COMMIT

--- server-stats.sh
+++ server-stats.sh
@@ -8,7 +8,8 @@ for i in {1..77};do
 	echo -n "-"
 done
 echo
-sleep 0.1
+
+
 echo -e "${RED}Disk usage${NC}"
 echo 
 df -h | awk '{print $1,$4,$5}' | column -t
@@ -20,5 +21,3 @@ echo
 echo -e "${RED}Top 5 processes by CPU and Memory usage${NC}"
 echo
 top | ps -aux | awk '{print $1,$3,$4,$11}' | head -6
-
-
